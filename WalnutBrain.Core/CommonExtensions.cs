e71cbc15fafe1651ff6c3588e35d9f7cc710838f
ijsgaus <ijsgaus@gmail.com>
2016-01-21 13:37:26 +0300

    CommonExtension:   AsFormat   In

---
+++ WalnutBrain.Core/CommonExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace WalnutBrain
+{
+    public static class CommonExtensions
+    {
+        public static string AsFormat(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+
+        public static bool In<T>(T item, params object[] set) => set.Any(p => Equals(item, p));
+        
+    }
+}
